8afa3af36b7bac289047a26601fa4e0f6262ef3a
xiaowei <thedawn2015@163.com>
2016-07-07 23:25:01 +0800

    drawview：画饼图

--- drawview/src/main/java/com/simon/drawview/model/PieData.java
+++ drawview/src/main/java/com/simon/drawview/model/PieData.java
@@ -19,4 +19,44 @@ public class PieData {
         this.name = name;
         this.value = value;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public float getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(float percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
 }
