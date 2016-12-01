56d8fcc2fbf2f561efd714cfcb6a680135493780
xiaowei <thedawn2015@163.com>
2016-12-01 19:11:14 +0800

    third_login：QQ登录Model

--- Simples/src/main/java/com/simon/simple/third_login/ThirdLoginActivity.java
+++ Simples/src/main/java/com/simon/simple/third_login/ThirdLoginActivity.java
@@ -5,12 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.simon.baseandroid.listener.BaseUiListener;
 import com.simon.baseandroid.listener.IUserInfoListener;
 import com.simon.baseandroid.model.QQUserInfoModel;
+import com.simon.baseandroid.util.LogUtil;
 import com.simon.baseandroid.util.TencentUtil;
 import com.simon.simple.R;
+import com.squareup.picasso.Picasso;
 import com.tencent.tauth.Tencent;
 
 import butterknife.BindView;
@@ -26,6 +30,12 @@ public class ThirdLoginActivity extends AppCompatActivity {
     public static String TAG = ThirdLoginActivity.class.getSimpleName();
     @BindView (R.id.login_btn_qq)
     Button loginBtnQq;
+    @BindView (R.id.login_image_header)
+    ImageView loginImageHeader;
+    @BindView (R.id.login_text_nick_name)
+    TextView loginTextNickName;
+    @BindView (R.id.login_text_gender)
+    TextView loginTextGender;
 
     private Tencent tencent;
 
@@ -40,12 +50,6 @@ public class ThirdLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_third_login);
         ButterKnife.bind(this);
 
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Tencent.onActivityResultData(requestCode, resultCode, data, new BaseUiListener(this, tencent, iUserInfoListener));
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @OnClick (R.id.login_btn_qq)
@@ -66,7 +70,26 @@ public class ThirdLoginActivity extends AppCompatActivity {
     private IUserInfoListener iUserInfoListener = new IUserInfoListener() {
         @Override
         public void onUserInfoResponse(QQUserInfoModel qqUserInfoModel, String msg) {
+            LogUtil.i(TAG, "onUserInfoResponse: msg=" + msg);
+            LogUtil.i(TAG, "onUserInfoResponse: nickName=" + qqUserInfoModel.getNickname());
+            LogUtil.i(TAG, "onUserInfoResponse: gender=" + qqUserInfoModel.getGender());
+            LogUtil.i(TAG, "onUserInfoResponse: header=" + qqUserInfoModel.getFigureurl_qq_2());
 
+            refreshViews();
         }
     };
+
+    /**
+     * 刷新
+     */
+    private void refreshViews() {
+//        Picasso.with(this)
+//                .load()
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Tencent.onActivityResultData(requestCode, resultCode, data, new BaseUiListener(this, tencent, iUserInfoListener));
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
